bea1ec0aacee2d6f843066d5589c220d8567b080
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2022-12-13 12:55:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "")
-BOT_TOKEN = os.environ.get('bot_token', "")
+AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "mongodb+srv://gssbotch3:changuess@cluster0.kcyhfzx.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = os.environ.get('bot_token', "5907093089:AAGFgTxZCG52uQo5y4heZ8bxxD8wf9z-WLc")
