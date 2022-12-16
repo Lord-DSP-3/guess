1f75e8ec926bed94daf2ada589a6505958a22289
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2022-12-16 20:40:05 +0530

    Update and rename test.Dockerfile.py to Dockerfile.py

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,4 +1,4 @@
-"""FROM python:latest
+FROM python:latest
 
 
 RUN git clone https://github.com/Ishu-Hinata/guessy.git /Akinator
@@ -6,5 +6,3 @@ WORKDIR /Akinator
 RUN python -m pip install --upgrade pip
 RUN python -m pip install --no-cache-dir -r /Akinator/requirements.txt
 CMD python3 __main__.py
-
-"""
